7c4df740ef651d247531758fc8b326e9d6db1dcd
Anderson-Pereira-Dev <pereira818@hotmail.com>
2022-08-25 21:44:54 -0300

    Criação do projeto

---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no consolo*/
+		System.out.println("Meu primeiro programa em Java");
+		System.out.println("Java é orientada a objeto");
+	}
+
+}
