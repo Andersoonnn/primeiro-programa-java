ff88f92d589f05a1abf799a1017549b1bc4a4b4c
Anderson-Pereira-Dev <pereira818@hotmail.com>
2022-08-25 22:14:44 -0300

    Alteração e criação da classe pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,22 @@
+
+
+public class Pessoa {
+	
+	private String nome;
+	private String sorenome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSorenome() {
+		return sorenome;
+	}
+	public void setSorenome(String sorenome) {
+		this.sorenome = sorenome;
+	}
+
+	
+	
+}
