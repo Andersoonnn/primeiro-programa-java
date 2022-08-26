74e6299871a6e9cee348af2b856e543cf563c406
Anderson-Pereira-Dev <pereira818@hotmail.com>
2022-08-26 20:47:32 -0300

    Adicionando campo data de nascimento

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -5,6 +5,13 @@ public class Pessoa {
 	private String nome;
 	private String sorenome;
 	private int idade;
+	private Date dataNascimento;
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 	public int getIdade() {
 		return idade;
 	}
